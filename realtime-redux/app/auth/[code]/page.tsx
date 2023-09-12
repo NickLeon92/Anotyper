51e26d488a9f5ab3e7c787766b37b11089299e9c
Nicolas Leon <nicolasleon@nicolass-mbp.mynetworksettings.com>
2023-09-12 13:14:48 -0400

    test

--- realtime-redux/app/auth/[code]/page.tsx
+++ realtime-redux/app/auth/[code]/page.tsx
@@ -42,7 +42,7 @@ useEffect(() => {
                 client_secret: clientSecret,
                 grant_type: 'authorization_code',
                 code: code,
-                redirect_uri: 'https://www.anotype.app/auth',
+               // redirect_uri: 'https://www.anotype.app/auth',
                 scope: 'identify', // Adjust scopes based on your app's requirements
             }
         })
